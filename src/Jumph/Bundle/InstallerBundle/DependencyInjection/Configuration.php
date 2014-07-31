4abf148a39ee2a06b74bd88b34bbe6694c470517
Peter Nijssen <peter@peternijssen.nl>
2014-07-31 21:31:52 +0200

    fix unused variables

--- src/Jumph/Bundle/InstallerBundle/DependencyInjection/Configuration.php
+++ src/Jumph/Bundle/InstallerBundle/DependencyInjection/Configuration.php
@@ -25,7 +25,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('jumph_installer');
+        $treeBuilder->root('jumph_installer');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
