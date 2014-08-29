a2cf71f0985fd62f50465a3e20642c45f3df2829
Peter Nijssen <peter@peternijssen.nl>
2014-08-29 23:28:02 +0200

    activity stream base

---
+++ src/Jumph/Bundle/DashboardBundle/Entity/ActivityStream.php
@@ -0,0 +1,178 @@
+<?php
+
+/**
+ * This file is part of the Jumph package.
+ *
+ * (c) Peter Nijssen
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Jumph\Bundle\DashboardBundle\Entity;
+
+use Jumph\Bundle\UserBundle\Entity\User;
+
+class ActivityStream {
+
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $objectId;
+
+    /**
+     * @var string
+     */
+    private $objectType;
+
+    /**
+     * @var string
+     */
+    private $verb;
+
+    /**
+     * @var \DateTime
+     */
+    private $createdAt;
+
+    /**
+     * @var User
+     */
+    private $user;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set objectId
+     *
+     * @param string $objectId
+     *
+     * @return ActivityStream
+     */
+    public function setObjectId($objectId)
+    {
+        $this->objectId = $objectId;
+
+        return $this;
+    }
+
+    /**
+     * Get objectId
+     *
+     * @return string 
+     */
+    public function getObjectId()
+    {
+        return $this->objectId;
+    }
+
+    /**
+     * Set objectType
+     *
+     * @param string $objectType
+     *
+     * @return ActivityStream
+     */
+    public function setObjectType($objectType)
+    {
+        $this->objectType = $objectType;
+
+        return $this;
+    }
+
+    /**
+     * Get objectType
+     *
+     * @return string 
+     */
+    public function getObjectType()
+    {
+        return $this->objectType;
+    }
+
+    /**
+     * Set verb
+     *
+     * @param string $verb
+     *
+     * @return ActivityStream
+     */
+    public function setVerb($verb)
+    {
+        $this->verb = $verb;
+
+        return $this;
+    }
+
+    /**
+     * Get verb
+     *
+     * @return string 
+     */
+    public function getVerb()
+    {
+        return $this->verb;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return ActivityStream
+     */
+    public function setCreatedAt(\DateTime $createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set user
+     *
+     * @param User $user
+     *
+     * @return ActivityStream
+     */
+    public function setUser(User $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+}
